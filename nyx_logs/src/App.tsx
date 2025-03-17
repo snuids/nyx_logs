2d5c9f7bb25289c780ad3476b8b7d84b2c3d92f6
Arnaud Marchand <amarchand@icloud.com>
2025-03-17 08:15:34 +0100

    Save point

---
+++ nyx_logs/src/App.tsx
@@ -0,0 +1,173 @@
+import { Button, Breadcrumb, Table,Row,Col } from "antd";
+import { SyncOutlined } from "@ant-design/icons";
+import type { ColumnsType } from "antd/es/table";
+
+import { useState, useEffect } from "react";
+import { FaFile, FaFolder } from "react-icons/fa"; // Import the file icon from react-icons
+import FileViewer from './FileViewer';
+import { Flex } from "antd";
+import "./App.css";
+
+interface FileDataType {
+  type: string;
+  name: string;
+  creation_time: number;
+  modification_time: number;
+  size: number;
+}
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [count, setCount] = useState(0);
+  const [token, setToken] = useState("");
+  const [api, setApi] = useState("");
+  const [apiData, setApiData] = useState([]);
+  const [basePath, setBasePath] = useState<string[]>([]); // Create basePath state
+  const [selectedFile, setSelectedFile] = useState<string | null>(null); // State to track the selected file
+
+  
+  const transformBasePath = (
+    basePath: string[]
+  ): { title: string; onClick: () => void }[] => {
+    return basePath.map((path, index) => ({
+      title: path.replace(/\//g, ""),
+      onClick: () => {
+        setBasePath(basePath.slice(0, index + 1));
+        setCount(count + 1); // Trigger the useEffect to refresh the data
+      },
+    }));
+  };
+
+  const handleFolderClick = (path: string) => {
+    setBasePath([...basePath, path]);
+    setCount(count + 1); // Trigger the useEffect to refresh the data
+  };
+  
+  const handleFileClick = (file: string) => {
+    setSelectedFile(file); // Set the selected file
+  };
+
+  const handleBack = () => {
+    setSelectedFile(null); // Clear the selected file to go back to the table
+  };
+
+  const columns: ColumnsType<FileDataType> = [
+    {
+      title: "Type",
+      dataIndex: "type",
+      key: "type",
+      render: (text: string, record: FileDataType) =>
+        text === "file" ? (
+          <FaFile onClick={() => handleFileClick(record.name)} style={{ cursor: "pointer" }} />
+        ) : (
+          <FaFolder
+            onClick={() => handleFolderClick(record.name)}
+            style={{ cursor: "pointer" }}
+          />
+        ), // Render file icon if type is 'file'
+      sorter: (a, b) => a.type.localeCompare(b.type),
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      sorter: (a, b) => a.name.localeCompare(b.name),
+    },
+    {
+      title: "Creation",
+      dataIndex: "creation_time",
+      key: "creation_time",
+      render: (text: number) => new Date(text * 1000).toLocaleString(),
+      sorter: (a, b) => (a.creation_time > b.creation_time ? 1 : -1),
+    },
+    {
+      title: "Modification",
+      dataIndex: "modification_time",
+      key: "modification_time",
+      render: (text: number) => new Date(text * 1000).toLocaleString(),
+      sorter: (a, b) => (a.modification_time > b.modification_time ? 1 : -1),
+    },
+    {
+      title: "Size",
+      dataIndex: "size",
+      key: "size",
+      sorter: (a, b) => (a.size > b.size ? 1 : -1),
+    },
+  ];
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const tokenParam = params.get("token");
+    if (tokenParam) {
+      setToken(tokenParam);
+    }
+    const apiParam = params.get("api");
+    if (apiParam) {
+      setApi(apiParam);
+    }
+    const basePath = params.get("path");
+    if (basePath) {
+      setBasePath([basePath]);
+    }
+  }, []);
+
+  useEffect(() => {
+    //refresh();
+    if (api && token) {
+      setLoading(true);
+
+      //alert(api+'listdir?token='+token);
+      fetch(api + "listdir?token=" + token, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ rec_id: -1, path: basePath.join("/") }),
+      })
+        .then((response) => response.json())
+        .then((data) => setApiData(data.data))
+        .then(() => setLoading(false))
+        .catch((error) => console.error("Error fetching API:", error));
+    }
+  }, [api, token, count, basePath]);
+
+  return (
+    <div style={{ width: "100%", padding: "20px",verticalAlign:"top" }}>
+      {!selectedFile &&  <Flex gap="middle" align="start" vertical style={{ marginBottom: 16 }}>
+        <Row align="middle" >
+          <Col style={{padding:"10px"}}>
+          <Button
+            type="primary"
+            icon={<SyncOutlined />}
+            onClick={() => setCount((count) => count + 1)}
+            loading={loading}
+          >
+            Refresh
+          </Button>
+          </Col>
+          <Col>
+          <Breadcrumb>
+            {transformBasePath(basePath).map((item, index) => (
+              <Breadcrumb.Item key={index} onClick={item.onClick}>
+                {item.title}
+              </Breadcrumb.Item>
+            ))}
+          </Breadcrumb>
+          </Col>
+        </Row>
+      </Flex>}
+      {selectedFile ? (
+        <FileViewer file={basePath.join("/")+"/"+selectedFile} onBack={handleBack}  api={api} token={token}/>
+      ) : (
+        <Table
+          dataSource={apiData}
+          columns={columns}
+          size="small"
+          style={{ width: "100%" }}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ nyx_logs/src/FileViewer.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+
+interface FileViewerProps {
+    file: string;
+    onBack: () => void;
+    api: string;
+    token: string;
+  }
+
+const FileViewer: React.FC<FileViewerProps> = ({ file, onBack,api, token }) => {
+  const [fileContent, setFileContent] = useState<string | null>(null);
+  const [logs, setLogs] = useState<string | null>(null);
+
+  
+  const setLogsData = (data: string) => {
+    setLogs( JSON.parse(data).data);
+  }
+  useEffect(() => {
+    const fetchFileContent = async () => {
+      try {
+        const response = await fetch(`${api}streamfile?token=${token}&file=${file}`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.text();
+        setFileContent(data);
+        setLogsData(data)
+      } catch (error) {
+        console.error('Error fetching file content:', error);
+      }
+    };
+
+    fetchFileContent();
+  }, [api, token, file]);
+
+  return (
+    <div padding="20px">
+      <button  onClick={onBack}>Back</button>
+      &nbsp;
+      <span>Viewing File: {file}</span>
+      {fileContent ? (
+        <div style={{fontFamily:"monospace",fontSize:"11px", overflow:"auto",textAlign:"left",position:"absolute",left:"10px",right:"10px",top:"80px",bottom:"10px",backgroundColor:"black",color:"white"}}
+         dangerouslySetInnerHTML={{ __html: logs?.replace(/INFO/g,"<span class=\"info\">INFO</span>")
+            .replace(/ERROR/g,"<span class=\"error\">ERROR</span>").replace(/\n/g, '<br>') || '' }} />
+        
+      ) : (
+        <p>Loading file content...</p>
+      )}
+    </div>
+  );
+};
+
+export default FileViewer;
